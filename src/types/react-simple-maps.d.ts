0142e7913412ead4040997bec488794b7399d5dc
ahmedgouda5 <ahmedgodayyy@gmail.com>
2025-09-08 18:36:31 +0300

    latest update

---
+++ src/types/react-simple-maps.d.ts
@@ -0,0 +1,9 @@
+declare module "react-simple-maps" {
+    import * as React from "react";
+  
+    export const ComposableMap: React.FC<Record<string, unknown>>;
+    export const Geographies: React.FC<Record<string, unknown>>;
+    export const Geography: React.FC<Record<string, unknown>>;
+    export const ZoomableGroup: React.FC<Record<string, unknown>>;
+  }
+  
